e5d76049cb6ec54cf2f462dd89204a63e8018c15
vant <hgtk11@163.com>
2018-12-02 21:33:58 +0800

    test connect to ssh server by putty

--- backend/src/main/java/org/wrolplin/app/MyWebSocketAdapter.java
+++ backend/src/main/java/org/wrolplin/app/MyWebSocketAdapter.java
@@ -1,17 +1,18 @@
 package org.wrolplin.app;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.PipedInputStream;
-import java.io.PipedOutputStream;
 import java.nio.ByteBuffer;
-import java.util.EnumSet;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import org.apache.sshd.client.SshClient;
-import org.apache.sshd.client.channel.ChannelShell;
-import org.apache.sshd.client.channel.ClientChannelEvent;
-import org.apache.sshd.client.session.ClientSession;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.WebSocketAdapter;
 
@@ -26,52 +27,91 @@ public class MyWebSocketAdapter extends WebSocketAdapter {
         super.onWebSocketConnect(sess);
         new Thread("transfer") {
             public void run() {
-                client = SshClient.setUpDefaultClient();
-                client.start();
-                try (ClientSession session = client.connect("gns3", "192.168.1.104", 22)
-                        .verify()
-                        .getSession()) {
-                    session.addPasswordIdentity("gns3");
-                    session.auth().verify();
-                    try (ChannelShell channel = session.createShellChannel();
-                            PipedInputStream inPis = new PipedInputStream();
-                            PipedOutputStream inPos = new PipedOutputStream(inPis);
-                            PipedInputStream outPis = new PipedInputStream();
-                            PipedOutputStream outPos = new PipedOutputStream(outPis)) {
+//                client = SshClient.setUpDefaultClient();
+//                client.start();
+//                try (ClientSession session = client.connect("gns3", "192.168.1.104", 22)
+//                        .verify()
+//                        .getSession()) {
+//                    session.addPasswordIdentity("gns3");
+//                    session.auth().verify();
+//                    try (ChannelShell channel = session.createShellChannel();
+//                            PipedInputStream inPis = new PipedInputStream();
+//                            PipedOutputStream inPos = new PipedOutputStream(inPis);
+//                            PipedInputStream outPis = new PipedInputStream();
+//                            PipedOutputStream outPos = new PipedOutputStream(outPis)) {
+//
+//                        channel.setUsePty(true);
+//                        channel.setPtyType("xterm");
+//                        channel.setIn(inPis);
+//                        channel.setOut(outPos);
+//                        fromServer = outPis;
+//                        toServer = inPos;
+//                        channel.setPtyWidth(40);
+//                        channel.setPtyHeight(50);
+//                        channel.open().verify();
+//
+//                        try {
+////                            (
+////                             BufferedWriter writer = new BufferedWriter(
+////                                new OutputStreamWriter(toServer, StandardCharsets.UTF_8));
+////                             BufferedReader reader = new BufferedReader(
+////                                new InputStreamReader(fromServer, StandardCharsets.UTF_8))
+////                            ) {
+////                            System.out.println("write");
+////                            writer.write("ls");
+////                            writer.write(0x0d);
+////                            writer.flush();
+////                            System.out.println("writed");
+//
+//                            byte[] buffer = new byte[1024];
+//                            int size;
+//                            while ((size = fromServer.read(buffer)) != -1) {
+//                                getSession().getRemote().sendBytes(ByteBuffer.wrap(buffer, 0, size));
+//                            }
+//                        } catch (Exception e) {
+//                            e.printStackTrace();
+//                        }
+//
+//                        channel.waitFor(EnumSet.of(ClientChannelEvent.CLOSED), 0);
+//                    }
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
 
-                        channel.setUsePty(true);
-                        channel.setPtyType("xterm");
-                        channel.setIn(inPis);
-                        channel.setOut(outPos);
-                        fromServer = outPis;
-                        toServer = inPos;
-                        channel.setPtyWidth(40);
-                        channel.setPtyHeight(50);
-                        channel.open().verify();
-
-                        try {
-//                            (
-//                             BufferedWriter writer = new BufferedWriter(
-//                                new OutputStreamWriter(toServer, StandardCharsets.UTF_8));
-//                             BufferedReader reader = new BufferedReader(
-//                                new InputStreamReader(fromServer, StandardCharsets.UTF_8))
-//                            ) {
-//                            System.out.println("write");
-//                            writer.write("ls");
-//                            writer.write(0x0d);
-//                            writer.flush();
-//                            System.out.println("writed");
-
-                            byte[] buffer = new byte[1024];
-                            int size;
-                            while ((size = fromServer.read(buffer)) != -1) {
-                                getSession().getRemote().sendBytes(ByteBuffer.wrap(buffer, 0, size));
-                            }
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-
-                        channel.waitFor(EnumSet.of(ClientChannelEvent.CLOSED), 0);
+                SimpleDateFormat formatter = new SimpleDateFormat("YYYYMMDDHHmmss");
+                String path = "F:/wrolp/session/" + formatter.format(new Date());
+                File file = new File(path);
+                try {
+                    new File("F:/wrolp/session").mkdirs();  
+                    file.createNewFile();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+                try (FileOutputStream toFile = new FileOutputStream(file)) {
+                    List<String> cmd = new ArrayList<>();
+                    cmd.add("F:/wrolp/putty/PLINK");
+                    cmd.add("-v");
+                    cmd.add("-a");
+                    cmd.add("-x");
+                    cmd.add("-l");
+                    cmd.add("cisco");
+                    cmd.add("-pw");
+                    cmd.add("cisco");
+                    cmd.add("-1");
+                    cmd.add("-P");
+                    cmd.add("22");
+                    cmd.add("192.168.1.15");
+                    ProcessBuilder ssh = new ProcessBuilder(cmd);
+                    ssh.redirectErrorStream(true);
+                    Process plink = ssh.start();
+                    toServer = plink.getOutputStream();
+                    fromServer = plink.getInputStream();
+                    byte[] buffer = new byte[1024];
+                    int size;
+                    while ((size = fromServer.read(buffer)) != -1) {
+                        getSession().getRemote().sendBytes(ByteBuffer.wrap(buffer, 0, size));
+                        toFile.write(buffer, 0, size);
+                        toFile.flush();
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
